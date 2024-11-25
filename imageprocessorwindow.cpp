c8132540cbf434d016d45d75f7b97e3591b2ff9d
iaald <the_iaald@163.com>
2024-11-26 00:30:24 +0800

    Image Processor GUI

--- imageprocessorwindow.cpp
+++ imageprocessorwindow.cpp
@@ -4,7 +4,9 @@
 #include <memory>
 #include <QMenu>
 #include <QFileDialog>
+#include <QMessageBox>
 #include <QSlider>
+#include <cstring>
 
 #include <iostream>
 
@@ -28,6 +30,7 @@ public:
     QGraphicsScene *currentScene = nullptr;
     QImage *procImage = nullptr;
     QImage *copImg = nullptr;
+    int initParas[14] = {0,10,10,10,0,10,10,0,0,0,0,10,10,10};
     int paras[14];
     bool haveSaved = true;
 
@@ -45,7 +48,7 @@ public:
                  qreal whiteBalanceRed,
                  qreal whiteBalanceGreen,
                  qreal whiteBalanceBlue){
-        imgProcessor->adjustImageParameters(*procImage,
+        imgProcessor->adjustImageParameters(*copImg,
                                             brightness,
                                             saturationFactor,
                                             vibranceFactor,
@@ -63,9 +66,7 @@ public:
     }
     void Initialize(){
         mode = -1;
-        for(int i = 0; i < 14; i++){
-            paras[i] = 0;
-        }
+        memcpy(paras,initParas,14*sizeof(int));
         delete currentScene;
         currentScene = nullptr;
         delete procImage;
@@ -129,44 +130,89 @@ ImageProcessorWindow::~ImageProcessorWindow()
     delete ui;
 }
 void ImageProcessorWindow::SetParaValue(int value){
-    if(ImageProcessorGUIController::GetInstance().mode==0){
-    ImageProcessorGUIController::GetInstance().paras[0] = value;
-    }else {
-
+    int t = ImageProcessorGUIController::GetInstance().mode;
+    if(t==0){
+        ImageProcessorGUIController::GetInstance().paras[0] = value;
+    }else if(t==1){
+        ImageProcessorGUIController::GetInstance().paras[1] = value;
+    }else if(t==2){
+        ImageProcessorGUIController::GetInstance().paras[2] = value;
+    }else if(t==3){
+        ImageProcessorGUIController::GetInstance().paras[3] = value;
+    }else if(t==4){
+        ImageProcessorGUIController::GetInstance().paras[4] = value;
+    }else if(t==6){
+        ImageProcessorGUIController::GetInstance().paras[7] = value;
+    }else if(t==7){
+        ImageProcessorGUIController::GetInstance().paras[8] = value;
+    }else if(t==8){
+        ImageProcessorGUIController::GetInstance().paras[9] = value;
+    }else if(t==9){
+        ImageProcessorGUIController::GetInstance().paras[10] = value;
     }
 }
+void ImageProcessorWindow::SetH(int value){
+    ImageProcessorGUIController::GetInstance().paras[5] = value;
+}
+void ImageProcessorWindow::SetS(int value){
+    ImageProcessorGUIController::GetInstance().paras[6] = value;
+}
+void ImageProcessorWindow::SetR(int value){
+    ImageProcessorGUIController::GetInstance().paras[11] = value;
+}
+void ImageProcessorWindow::SetG(int value){
+    ImageProcessorGUIController::GetInstance().paras[12] = value;
+}
+void ImageProcessorWindow::SetB(int value){
+    ImageProcessorGUIController::GetInstance().paras[13] = value;
+}
+
 void ImageProcessorWindow::on_LoadButton_clicked()
 {
     if(!ImageProcessorGUIController::GetInstance().haveSaved){
         //TODO:提示未保存
-        std::cout << "YOU DIDNT SAVE!!" << std::endl;
+        QMessageBox::StandardButton a = QMessageBox::information(NULL, "Image Processor", "Do you want to save current change of the file?", QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel, QMessageBox::Yes);
+        if(a==QMessageBox::StandardButton::Cancel){
+            return;
+        }else if (a==QMessageBox::StandardButton::Yes){
+            on_SaveButton_clicked();
+            return;
+        }
     }
     changeState(-1);
     ImageProcessorGUIController::GetInstance().Initialize();
-    QString filename = QFileDialog::getOpenFileName(this,tr("Open File"),"",tr("File (*.bmp)"));
+    QString filename = QFileDialog::getOpenFileName(this,tr("Open File"),"",tr("Image files(*.bmp *.jpg *.pbm *.pgm *.png *.ppm *.xbm *.xpm)"));
     if(filename.toStdString()==""){
         return;
     }
-    std::cout << "Loaded: " <<filename.toStdString() << std::endl;
     if(ImageProcessorGUIController::GetInstance().procImage!=nullptr){
         delete ImageProcessorGUIController::GetInstance().procImage;
     }
     ImageProcessorGUIController::GetInstance().procImage = new QImage;
     if(ImageProcessorGUIController::GetInstance().procImage->load(filename)){
         ImageProcessorGUIController::GetInstance().ShowImage(ui->graphicsView,this);
+        this->filename = filename;
+//        std::cout << "Loaded: " <<filename.toStdString() << std::endl;
     }else{
-        std::cout << "An Unknow Problem Happened." << std::endl;
+//        std::cout << "An Unknow Problem Happened." << std::endl;
     }
 }
 
 void ImageProcessorWindow::on_SaveButton_clicked()
 {
-
+    if(ImageProcessorGUIController::GetInstance().procImage==nullptr||this->filename.isEmpty()){
+        return;
+    }
+    QString t = this->filename.left(this->filename.lastIndexOf("."));
+    QString res = QFileDialog::getSaveFileName(this,tr("Save File"),t.append("(1)"),tr("BMP (*.bmp);;JPG (*.jpg);;PNG (*.png)"));
+//    std::cout << res.toStdString() << std::endl;
+    ImageProcessorGUIController::GetInstance().copImg->save(res);
+    ImageProcessorGUIController::GetInstance().haveSaved = true;
 }
 
 void ImageProcessorWindow::on_pushButton_2_clicked()
 {
-
+    //Use menu to select mode, nothing happens here.
 }
 void ImageProcessorWindow::changeState(int i){
     while(ui->verticalLayout->count())
@@ -186,7 +232,7 @@ void ImageProcessorWindow::onBrightnessProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(-255);
     slider->setMaximum(255);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[0]);
 
     ui->verticalLayout->addWidget(slider);
     connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
@@ -199,9 +245,10 @@ void ImageProcessorWindow::onSaturationProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(0);
     slider->setMaximum(20);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[1]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::onVibranceProc(){
     changeState(2);
@@ -211,9 +258,10 @@ void ImageProcessorWindow::onVibranceProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(0);
     slider->setMaximum(20);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[2]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::onContrastProc(){
     changeState(3);
@@ -223,9 +271,10 @@ void ImageProcessorWindow::onContrastProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(5);
     slider->setMaximum(30);
-    slider->setValue(5);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[3]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::onClarityProc(){
     changeState(4);
@@ -235,9 +284,10 @@ void ImageProcessorWindow::onClarityProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(-100);
     slider->setMaximum(100);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[4]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::onHighlightShadowProc(){
     changeState(5);
@@ -247,14 +297,16 @@ void ImageProcessorWindow::onHighlightShadowProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(0);
     slider->setMaximum(20);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[5]);
     QSlider* slider2(new QSlider(this));
     slider2->setOrientation(Qt::Orientation::Horizontal);
     slider2->setMinimum(0);
     slider2->setMaximum(20);
-    slider2->setValue(0);
+    slider2->setValue(ImageProcessorGUIController::GetInstance().paras[6]);
     ui->verticalLayout->addWidget(slider);
     ui->verticalLayout->addWidget(slider2);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetH);
+    connect(slider2, &QSlider::valueChanged, this,&ImageProcessorWindow::SetS);
 };
 void ImageProcessorWindow::onTemperatureProc(){
     changeState(6);
@@ -264,9 +316,10 @@ void ImageProcessorWindow::onTemperatureProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(-100);
     slider->setMaximum(100);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[7]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::onHueProc(){
     changeState(7);
@@ -276,9 +329,10 @@ void ImageProcessorWindow::onHueProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(-180);
     slider->setMaximum(180);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[8]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::featherEdgesProc(){
     changeState(8);
@@ -288,9 +342,10 @@ void ImageProcessorWindow::featherEdgesProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(0);
     slider->setMaximum(50);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[9]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::addGainProc(){
     changeState(9);
@@ -300,9 +355,10 @@ void ImageProcessorWindow::addGainProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(0);
     slider->setMaximum(10);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[10]);
 
     ui->verticalLayout->addWidget(slider);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetParaValue);
 };
 void ImageProcessorWindow::whiteBalanceProc(){
     changeState(10);
@@ -312,23 +368,26 @@ void ImageProcessorWindow::whiteBalanceProc(){
     slider->setOrientation(Qt::Orientation::Horizontal);
     slider->setMinimum(5);
     slider->setMaximum(20);
-    slider->setValue(0);
+    slider->setValue(ImageProcessorGUIController::GetInstance().paras[11]);
 
     QSlider* slider1(new QSlider(this));
     slider1->setOrientation(Qt::Orientation::Horizontal);
     slider1->setMinimum(5);
     slider1->setMaximum(20);
-    slider1->setValue(0);
+    slider1->setValue(ImageProcessorGUIController::GetInstance().paras[12]);
 
     QSlider* slider2(new QSlider(this));
     slider2->setOrientation(Qt::Orientation::Horizontal);
     slider2->setMinimum(5);
     slider2->setMaximum(20);
-    slider2->setValue(0);
+    slider2->setValue(ImageProcessorGUIController::GetInstance().paras[13]);
 
     ui->verticalLayout->addWidget(slider);
     ui->verticalLayout->addWidget(slider1);
     ui->verticalLayout->addWidget(slider2);
+    connect(slider, &QSlider::valueChanged, this,&ImageProcessorWindow::SetR);
+    connect(slider1, &QSlider::valueChanged, this,&ImageProcessorWindow::SetG);
+    connect(slider2, &QSlider::valueChanged, this,&ImageProcessorWindow::SetB);
 };
 
 
@@ -338,10 +397,12 @@ void ImageProcessorWindow::on_pushButton_pressed()
     if(ImageProcessorGUIController::GetInstance().procImage==nullptr){
         return;
     }
+    delete ImageProcessorGUIController::GetInstance().copImg;
     ImageProcessorGUIController::GetInstance().copImg = new QImage(*ImageProcessorGUIController::GetInstance().procImage);
-    ImageProcessorGUIController::GetInstance().imgProcessor->adjustBrightness(
-        *ImageProcessorGUIController::GetInstance().copImg,
-        ImageProcessorGUIController::GetInstance().paras[0]
-    );
+    int (&paras)[14] = ImageProcessorGUIController::GetInstance().paras;
+    ImageProcessorGUIController::GetInstance().Process(
+            paras[0],paras[1]/10.0f,paras[2]/10.0f,paras[3]/10.0f,paras[4],
+            paras[5]/10.0f,paras[6]/10.0f,paras[7],paras[8],paras[9],
+            paras[10]/10.0f,paras[11]/10.0f,paras[12]/10.0f,paras[13]/10.0f);
     ImageProcessorGUIController::GetInstance().ShowImage(ui->graphicsView,this);
 }
