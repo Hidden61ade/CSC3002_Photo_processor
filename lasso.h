3e3b0bd49e72164b93060cde2c6152397f4c04d2
Zhang Yang <123090836@link.cuhk.edu.cn>
2024-12-07 20:33:24 +0800

    Lasso写成类
    完成

--- lasso.h
+++ lasso.h
@@ -1,13 +1,38 @@
 #ifndef LASSO_H
 #define LASSO_H
 
-
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include<vector>
+#include <vector>
 
-// 鼠标回调函数
-void mouseCallback(int event, int x, int y, int flags, void* userdata);
+class Lasso {
+public:
+    Lasso(const std::string& imagePath);
+    void start();
 
-#endif // MAIN_H
+private:
+    cv::Mat image, originalImage;
+    std::vector<cv::Point> points;
+    bool isDrawing;
+
+    static Lasso* instance;  // 静态指针用于存储类实例
+
+    static void mouseCallback(int event, int x, int y, int flags, void* userdata);
+
+    void processMouseCallback(int event, int x, int y, int flags, void* userdata);
+};
+
+#endif // LASSO_H
+//#ifndef LASSO_H
+//#define LASSO_H
+
+
+
+//#include <opencv2/opencv.hpp>
+//#include <iostream>
+//#include<vector>
+
+//// 鼠标回调函数
+//void mouseCallback(int event, int x, int y, int flags, void* userdata);
+
+//#endif // MAIN_H
--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 #include <QApplication>
 
+#include "lasso.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     printf("hello");
+//    Lasso lasso("D:/11111.jpg");
+//    lasso.start();
+
     return a.exec();
 }
